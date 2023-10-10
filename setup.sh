25000e6e09b011ff2c0b81c27147613032472022
Con Gà Rô <142249913+congaro75@users.noreply.github.com>
2023-10-10 14:30:20 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
 #! /usr/bin/bash
 #! Ho Ngoc Hai, don't touch baby
-
-apt update
 pip3 install -r requirements.txt &>/dev/null
 pip3 install --upgrade requests
-mv -v * /content/Fshare-Download-To-GG-Driver/ ../
+mv -v * ../
 
